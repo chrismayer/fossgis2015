9053ab9e792873518759960bc86b4a4063a086de
Christian Mayer <chris@meggsimum.de>
2015-03-10 18:40:15 +0100

    Finished ol3 / nodejs example

--- nodejs-examples/demo-server.js
+++ nodejs-examples/demo-server.js
@@ -4,27 +4,84 @@ var fs = require('fs');
 // 3rd party deps
 var turf = require('turf');
 var express = require('express');
+var request = require('request');
+var xmldoc = require('xmldoc');
 
+// the web app
 var app = express();
 
 // the web service for data delivery
 app.get('/data', function(req, res) {
 
-    // create a simple point object \w turf wrapper
-    var out = turf.point([7.613053321838379, 51.96359873687842]);
-//    var out = turf.point([0, 0]);
-//    out = turf.featurecollection([out]);
+    // create current date - 10 minutes
+    // form 2015-03-10T10:40:00
+    var now = new Date(),
+        MS_PER_MINUTE = 60000,
+        reqDate = new Date(now - 10 * MS_PER_MINUTE),
+        // format the date
+        reqDateString =
+        reqDate.getFullYear() + '-0' + (reqDate.getMonth()+1) + '-' + reqDate.getDate() + 'T' +
+        reqDate.getHours() + ':' + reqDate.getMinutes() + ':' + reqDate.getSeconds();
 
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.write(JSON.stringify(out));
-    res.end();
+    // request the latest OSM changesets
+    request({
+        url: "http://api.openstreetmap.org//api/0.6/changesets?time=" + reqDateString,
+        method: "GET"
+    }, function (error, response, body) {
+
+        if (!error && response.statusCode == 200) {
+
+            // parse the XML response of OSM
+            var document = new xmldoc.XmlDocument(body);
+
+            var changesets = document.children,
+                csGeojson = [],
+                fcOut;
+
+            // iterate over all OSM changesets
+            for (var int = 0; int < changesets.length; int++) {
+                var cs = changesets[int];
+                if(cs.attr.min_lat) {
+                    var point = turf.point([
+                        parseFloat(cs.attr.min_lon, 10),
+                        parseFloat(cs.attr.min_lat, 10)
+                    ]);
+                    var polygon = turf.polygon([[
+                        [parseFloat(cs.attr.min_lon, 10),
+                         parseFloat(cs.attr.min_lat, 10)],
+                        [parseFloat(cs.attr.min_lon, 10),
+                         parseFloat(cs.attr.max_lat, 10)],
+                        [parseFloat(cs.attr.max_lon, 10),
+                         parseFloat(cs.attr.max_lat, 10)],
+                        [parseFloat(cs.attr.max_lon, 10),
+                         parseFloat(cs.attr.min_lat, 10)],
+                        [parseFloat(cs.attr.min_lon, 10),
+                         parseFloat(cs.attr.min_lat, 10)],
+                    ]]);
+                    var pointOnPolygon = turf.pointOnSurface(polygon);
+                    // create envelope --> point on surface as
+                    csGeojson.push(pointOnPolygon);
+                    fcOut = turf.featurecollection(csGeojson);
+                }
+            }
+
+            res.writeHead(200, {'Content-Type': 'application/json'});
+            res.write(JSON.stringify(fcOut));
+            res.end();
+
+        } else {
+            res.writeHead(500, {'Content-Type': 'application/json'});
+            res.write(JSON.stringify({error: true}));
+            res.end();
+        }
+    });
+
 });
 
 /*
  * URL binding for the demo ol3 app
  */
 app.get('/app', function(req, res) {
-    
     fs.readFile(__dirname + '/ol3-example/index.html', function(err, page) {
         res.writeHead(200, {'Content-Type': 'text/html'});
         res.write(page);
@@ -34,7 +91,6 @@ app.get('/app', function(req, res) {
 app.get('/:name', function(req, res) {
     var fileName = req.params.name;
     if (fs.existsSync(__dirname + '/ol3-example/' + fileName)) {
-        // Do something
         fs.readFile(__dirname + '/ol3-example/' + fileName, function(err, page) {
             res.writeHead(200, {'Content-Type': 'text/html'});
             res.write(page);
@@ -47,14 +103,11 @@ app.get('/:name', function(req, res) {
 });
 app.get('/libs/:name', function(req, res) {
     var fileName = req.params.name;
-    console.log("what2", __dirname + '/ol3-example/' + fileName);
-  
-  fs.readFile(__dirname + '/ol3-example/libs/' + fileName, function(err, page) {
-      res.write(page);
-      res.end();
-  });
+    fs.readFile(__dirname + '/ol3-example/libs/' + fileName, function(err, page) {
+        res.write(page);
+        res.end();
+    });
 });
 
-
-
+// start the server
 var server = app.listen(9999);
--- nodejs-examples/ol3-example/app.js
+++ nodejs-examples/ol3-example/app.js
@@ -1,25 +1,14 @@
-//var map;
+var map;
+/**
+ * Init the ol3 map
+ */
 function init() {
-    var vs = new ol.source.GeoJSON({
+    var vectorsrc = new ol.source.GeoJSON({
         projection: 'EPSG:3857',
         url: '/data'
-//        object: {
-//            'type': 'FeatureCollection',
-//            'crs': {
-//              'type': 'name',
-//              'properties': {
-//                'name': 'EPSG:4326'
-//              }
-//            },
-//            'features': [
-//              {
-//                'type': 'Feature',
-//                'geometry': {
-//                  'type': 'Point',
-//                  'coordinates': [100, 8]
-//                }
-//              }]
-//        }
+    });
+    var vectorLayer = new ol.layer.Vector({
+        source: vectorsrc
     });
     map = new ol.Map({
         target: 'map',
@@ -27,45 +16,74 @@ function init() {
             new ol.layer.Tile({
                 source: new ol.source.OSM()
             }),
-            new ol.layer.Vector({
-                source: vs,
-                style: sf
-            })
+            vectorLayer
         ],
         view: new ol.View({
             center: ol.proj.transform([7.613053321838379, 51.96359873687842], 'EPSG:4326', 'EPSG:3857'),
             zoom: 2
         })
     });
+
+    animiere();
+    // reload new data every 15 seconds
+    setInterval(reloadData, 15000);
 }
 
-function sf(feature, resolution) {
-    var text = 'pter';
-    return [new ol.style.Style({
-          image: new ol.style.Circle({
-              radius: 5,
-              fill: null,
-              stroke: new ol.style.Stroke({color: 'red', width: 1})
+/**
+ * Shows new data from the web service by adding a new
+ * vector layer. Always keeps the layer with the data before.
+ */
+function reloadData() {
+    
+    animiere();
+
+    var vector = new ol.layer.Vector({
+        source: new ol.source.GeoJSON({
+            projection: 'EPSG:3857',
+            url: '/data'
+        }),
+        style: new ol.style.Style({
+            image: new ol.style.Circle({
+                radius: 5,
+                fill: null,
+                stroke: new ol.style.Stroke({
+                    color: getRandomColor(),
+                    width: 2
+                })
             })
-    })];
-    return highlightStyleCache[text];
+      })
+    });
+    if (map.getLayers().getLength() > 2) {
+        map.getLayers().removeAt(1);
+    }
+    map.getLayers().push(vector);
 }
 
-function loadService() {
-    var url = '/data';
-  $.ajax({
-      url: url,
-      dataType: 'json',
-      success: function(response) {
-          
-          var vector = new ol.layer.Vector({
-              source: new ol.source.GeoJSON({
-                  projection: 'EPSG:3857',
-                  url: '/data'
-              })
-            });
-          
-          map.addLayer(vector);
-      }
-    });
+/**
+ * Returns a rendom hex color
+ */
+function getRandomColor() {
+    var letters = '0123456789ABCDEF'.split('');
+    var color = '#';
+    for (var i = 0; i < 6; i++ ) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+/**
+ * 
+ */
+function animiere() {
+    var ladebalken = document.getElementById("ladebalken");
+    var laenge = parseInt(ladebalken.style.width);
+
+    laenge++;
+    ladebalken.style.width = laenge + "px";
+
+    if (laenge < 300) {
+        window.setTimeout(animiere, 50);
+    } else {
+        ladebalken.style.width = '0';
+    }
 }
